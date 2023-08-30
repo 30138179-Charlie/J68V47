421f85e415b987c57b5b7cf8a37d41c7a85b126d
30138179 <30138179@nescol.ac.uk>
2023-08-30 15:48:05 +0100

    .

---
+++ src/Exercise1.java
@@ -0,0 +1,27 @@
+public class Exercise1 {
+    public static void main(String[] args) {
+        String haiku1 = "Breeze through summer trees\nWhispering leaves sing softly\nNature's lullaby\n";
+        String haiku2 = "Walking in moonlight\nSilver light guides my footsteps\nAlone but not lonely";
+        String haiku3 = "First day of autumn\nLeaves changing, cool winds blowing\nEarth prepares for rest";
+
+        System.out.println(haiku1);
+        System.out.println(haiku1);
+        System.out.println(haiku1);
+
+        String anagram1 = "Regretful";
+        String anagram2 = "Masterful";
+        String anagram3 = "Laughable";
+        String anagram4 = "Increasing";
+        String anagram5 = "Computing";
+        String anagram1_answer = "ferrel gut";
+        String anagram2_answer = "Artus Flem";
+        String anagram3_answer = "Able laugh";
+        String anagram4_answer = "Ace rinsing";
+        String anagram5_answer = "Coming put";
+        System.out.println(anagram1 +"|" + anagram1_answer);
+        System.out.println(anagram2 +"|" + anagram2_answer);
+        System.out.println(anagram3 +"|" + anagram3_answer);
+        System.out.println(anagram4 +"|" + anagram4_answer);
+        System.out.println(anagram5 +"|" + anagram5_answer);
+    }
+}
