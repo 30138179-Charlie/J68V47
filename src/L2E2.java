1646484c4c1deedc0e0f5d3a51a787c3143836ba
30138179 <30138179@nescol.ac.uk>
2023-09-06 15:35:21 +0100

    Lesson 2 Exercise 2

---
+++ src/L2E2.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class L2E2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num1 = input.nextInt();
+
+
+        System.out.print("Enter another number: ");
+        int num2 = input.nextInt();
+
+        System.out.println(num1 + num2);
+        System.out.println(num1 - num2);
+        System.out.println(num1 * num2);
+        System.out.println(num1 / num2);
+        System.out.println(num1 % num2);
+
+    }
+}
