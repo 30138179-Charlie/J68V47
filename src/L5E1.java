acfe4b940fe119f6a091502b58c9b918bc4e8704
30138179 <30138179@nescol.ac.uk>
2023-09-27 13:51:08 +0100

    .

--- src/L5E1.java
+++ src/L5E1.java
@@ -1,9 +1,12 @@
+import java.util.*;
 public class L5E1 {
     public static void main(String[] args) {
-        int subjectNumber = 11;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int subjectNumber = input.nextInt();
 
         for (int count = 1; count <13; count+=1) {
-            System.out.println("11 x " + String.valueOf(count) + " = "  + String.valueOf(subjectNumber * count));
+            System.out.println(String.valueOf(subjectNumber) + " x " + String.valueOf(count) + " = "  + String.valueOf(subjectNumber * count));
         }
     }
 }
