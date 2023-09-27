b2c79d8e653d23ab779b5a4c3d3c8d31770f0654
30138179 <30138179@nescol.ac.uk>
2023-09-27 13:36:01 +0100

    .

---
+++ src/L5E1.java
@@ -0,0 +1,9 @@
+public class L5E1 {
+    public static void main(String[] args) {
+        int subjectNumber = 11;
+
+        for (int count = 1; count <13; count+=1) {
+            System.out.println("11 x " + String.valueOf(count) + " = "  + String.valueOf(subjectNumber * count));
+        }
+    }
+}
