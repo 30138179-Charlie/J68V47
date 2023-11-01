39b40ee115223afe055107b3ba0452a8e2f1e4db
30138179 <30138179@nescol.ac.uk>
2023-11-01 14:11:06 +0000

    .

---
+++ src/L8E1.java
@@ -0,0 +1,10 @@
+public class L8E1 {
+    public static void main(String[] args) {
+
+        String[] students = {"Findlay", "Earyn", "Oliver", "Danny", "Amanda"};
+        System.out.println(students[0]);
+        System.out.println(students[4]);
+        System.out.println(students.length);
+    }
+}
+
